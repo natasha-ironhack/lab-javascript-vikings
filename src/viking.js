f53fe88f7ebdc2b6bf20283338efbedaa23c3fd7
Natasha Smith <pisceanchic@gmail.com>
2021-06-17 21:27:06 +0200

    pushing or adding to github for now

--- src/viking.js
+++ src/viking.js
@@ -1,13 +1,128 @@
 // Soldier
+/*
+Modify the Soldier class and add 2 methods to it: attack(), and receiveDamage().
+
+class
+should receive 2 arguments (health & strength)
+should receive the health property as its 1st argument
+should receive the strength property as its 2nd argument
+attack() method
+should be a function
+should receive 0 arguments
+should return the strength property of the Soldier
+receiveDamage() method
+should be a function
+should receive 1 argument (the damage)
+should remove the received damage from the health property
+shouldn't return anything
+*/
+
 class Soldier {}
+constructor (health, strength) {
+
+}
+
+
+
+
 
 // Viking
+
+/*
+A Viking is a Soldier with an additional property, their name. They also have 
+a different receiveDamage() method and new method, battleCry().
+
+Modify the Viking class, have it inherit from Soldier, re-implement the 
+receiveDamage() method for Viking, and add a new battleCry() method.
+
+inheritance
+Viking should extend Soldier
+
+class
+should receive 3 arguments (name, health & strength)
+should receive the name property as its 1st argument
+should receive the health property as its 2nd argument
+should receive the strength property as its 3rd argument
+
+attack() method
+(This method should be inherited from Soldier, no need to re-implement it.)
+
+should be a function
+should receive 0 arguments
+should return the strength property of the Viking
+
+receiveDamage() method
+(This method needs to be re-implemented for Viking because the Viking version 
+  needs to have different return values.)
+
+should be a function
+should receive 1 argument (the damage)
+should remove the received damage from the health property
+if the Viking is still alive, it should return "NAME has received DAMAGE points 
+of damage"
+if the Viking dies, it should return "NAME has died in act of combat"
+
+battleCry() method
+(Learn more about battle cries.) (link)
+
+should be a function
+should receive 0 arguments
+should return "Odin Owns You All!"
+
+*/
 class Viking {}
 
+
+
+
+
+
 // Saxon
+
+/*
+A Saxon is a weaker kind of Soldier. Unlike a Viking, a Saxon has no name. 
+Their receiveDamage() method will also be different than the original Soldier 
+version.
+
+Modify the Saxon, constructor function, have it inherit from Soldier and 
+re-implement the receiveDamage() method for Saxon.
+
+inheritance
+Saxon should extend Soldier
+
+class
+You don't have to include constructor method since this class will inherit 
+perfectly from the parents class, both, the health and the strength 
+(it extends Soldier class 😉 )
+
+attack() method
+(This method should be inherited from Soldier, no need to re-implement it.)
+
+should be a function
+should receive 0 arguments
+should return the strength property of the Saxon
+
+receiveDamage() method
+(This method needs to be re-implemented for Saxon because the Saxon version 
+  needs to have different return values.)
+
+should be a function
+should receive 1 argument (the damage)
+should remove the received damage from the health property
+if the Saxon is still alive, it should return "A Saxon has received DAMAGE
+ points of damage"
+if the Saxon dies, it should return "A Saxon has died in combat"
+*/
+
 class Saxon {}
 
-// War
+
+
+
+
+
+// BONUT: War
+
 class War {}
 
 
