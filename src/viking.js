25fffb40c2111f6ab9e94dd84c48c346e4ef7654
Natasha Smith <pisceanchic@gmail.com>
2021-06-21 15:35:00 +0200

    Solved most

--- src/viking.js
+++ src/viking.js
@@ -61,11 +61,57 @@ constructor (health, strength) {
   }
 
   // Saxon
-  class Saxon {}
+  class Saxon extends Soldier {
+    attack() {
+      return this.strength
+    }
+    receiveDamage(damage) {
+      this.health = this.health - damage;
+      if (this.health > 0) {
+        return `A Saxon has received ${damage} points of damage`
+      }
+      else if (this.health === 0) {
+        return `A Saxon has died in combat`
+      }
+  }
+}
   
   
   // War
-  class War {}
+  class War {
+    constructor () {
+      this.vikingArmy = [],
+      this.saxonArmy = []
+    }
+    addViking(viking) {
+      this.vikingArmy.push(viking);
+    }
+
+    addSaxon(saxon) {
+      this.saxonArmy.push(saxon);
+    }
+
+    vikingAttack() {
+      saxon.receiveDamage() == viking.strength
+    }
+
+    saxonAttack () {
+      viking.receiveDamage() == saxon.strength
+    }
+
+    showStatus () {
+      if (this.saxonArmy === 0) {
+        return `Vikings have won the war of the century!`
+      }
+      else if (this.vikingArmy === 0) {
+        return `Saxons have fought for their lives and survived another day...`
+      }
+      else if (this.saxonArmy >= 1 && this.vikingArmy >= 1) {
+        return `Vikings and Saxons are still in the thick of battle.`
+      }
+    }
+
+  }
 
 
   
